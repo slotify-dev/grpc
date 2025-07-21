c81abc9792527d5132d2cdb829dea1e148b93d0c
Sandip Patel <sandip254@gmail.com>
2025-07-21 17:28:20 -0400

    removed all files

--- src/utils/grpc-types.ts
+++
@@ -1,8 +0,0 @@
-import type { ProtoGrpcType } from '../generated/example_grpc_pb';
-
-export function isProtoGrpcType(obj: unknown): obj is ProtoGrpcType {
-  return obj !== null && 
-         typeof obj === 'object' && 
-         'example' in obj && 
-         typeof obj.example === 'object';
-}
